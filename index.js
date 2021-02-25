7535905e9c4d376043d3a2d4183f1087f12d0565
lismel rodriguez <lismel.rodriguez@gmail.com>
2021-02-25 17:06:54 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// alert ("zintis was here")//invoke a function
+var firstName = "Lismel"// assigning a string to the variable first name
+var lastName = "Rodriguez"
+var age = 33
+alert ("My name is " + firstName + " "+lastName)
+alert ("My age is " + age)
+console.log ("This class is pretty tough but I will not give up")
